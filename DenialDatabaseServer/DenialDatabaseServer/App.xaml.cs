62fd92888a850d37c1679c614d932bceda85d158
EmptyChapter <east908056@gmail.com>
2022-03-05 22:08:21 +0500

    Beginnings

---
+++ DenialDatabaseServer/DenialDatabaseServer/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace DenialDatabaseServer
+{
+    /// <summary>
+    /// Application core
+    /// </summary>
+    public partial class App : Application
+    {
+        /// <summary>
+        /// Raises on <see cref="Application.Startup"/> event
+        /// </summary>
+        /// <param name="e">Startup arguments</param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow();
+
+            MainWindow.Show();
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Raises on <see cref="Application.Exit"/> event
+        /// </summary>
+        /// <param name="e">Exit arguments</param>
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+        }
+    }
+}
